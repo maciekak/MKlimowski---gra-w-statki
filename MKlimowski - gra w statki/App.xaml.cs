5f3463d96675a0cfba0ac4f6c4d8c151331c2649
Maciek Klimowski <maciek.klimowski44@gmail.com>
2016-12-27 14:48:19 +0100

    Add project files.

---
+++ MKlimowski - gra w statki/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MKlimowski___gra_w_statki
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
